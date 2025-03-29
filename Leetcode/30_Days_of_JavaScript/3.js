9517d636fa4f349ab9e1f44ecf7d0be7b7df1c8d
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-03-29 15:31:53 -0500

    Updated directory structure

--- Leetcode/30_Days_of_JavaScript/3.js
+++ Leetcode/30_Days_of_JavaScript/3.js
@@ -1,8 +1,1 @@
-// Alvin's Recipe to Success
-
-// 1. Do problems in order
-// 2. read prompt (POLYA)
-// 3. Attempt problem. If lost, watch APPROACH
-// 4. Write code and test
-// 5. If stuck, watch WALKTHROUGH
-// 6. redo problems until you don't need to watch the approach or walkthrough to solve it
+// Leetcode problem 3
---
+++ structy/hey_programmer.js
@@ -0,0 +1,7 @@
+// Write a function greet that takes in a string argument, s, and returns the string "hey s". No tricks here. Run the tests to check your work.
+
+const greet = (s) => {
+    return `hey ${s}`
+}
+
+console.log(greet("Joshua"))
